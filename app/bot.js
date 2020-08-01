e46729a147fb137aeed06e16bb9602507f874bae
Alex Gonzalez <spokenbird@gmail.com>
2020-07-31 20:49:42 -0700

    Add Nodemon

--- app/bot.js
+++ app/bot.js
@@ -2,9 +2,8 @@ const { prefix } = require('../config.json');
 const mvps = require('../mvps.json');
 const moment = require('moment');
 const spellCheck = require('simple-spellchecker');
-const dictionary = spellCheck.getDictionarySync("mvpNames");
+const dictionary = spellCheck.getDictionarySync('mvpNames', './dict');
 
-console.log(dictionary);
 
 class Bot {
 
@@ -23,16 +22,26 @@ class Bot {
     static setTimer(command) {
         const userInputMvp = command.args[0].slice(1, -1);
         const isSpelledCorrectly = dictionary.spellCheck(userInputMvp)
-        // if (isSpelledCorrectly) {
-        //     for (const mvp in mvps) {
-        //         const mvpInfo = mvps[mvp];
-        //         for (const name of mvpInfo.names) {
-        //             if (name === userInputMvp) console.log(userInputMvp);
-        //         }
-        //     }
-        // } else {
-        //     console.log(dictionary.getSuggestions(userInputMvp, 2, 2));
-        // }
+        if (isSpelledCorrectly) {
+            for (const mvp in mvps) {
+                const mvpInfo = mvps[mvp];
+                for (const name of mvpInfo.names) {
+                    if (name === userInputMvp) console.log(userInputMvp);
+                }
+            }
+        } else {
+            const suggestions = dictionary.getSuggestions(userInputMvp, 5, 2);
+            let response = `I don't have an MVP with that name, did you mean ${suggestions[0]}?`
+            if (suggestions.length > 1) {
+                let prefix = " You can also say"
+                for (let i = 1; i < suggestions.length; i++) {
+                    prefix.concat(`${suggestions[i]}`);
+                }
+            }
+
+            console.log(response.concat(prefix));
+            return suggestions + prefix;
+        }
     }
 }
 
