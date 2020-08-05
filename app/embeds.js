d1838b52a8b8775b6bc3c53a3fd378b3b91006b0
Alex Gonzalez <spokenbird@gmail.com>
2020-08-05 12:42:36 -0700

    Update Help Embded language

--- app/embeds.js
+++ app/embeds.js
@@ -6,7 +6,7 @@ const helpEmbed = new Discord.MessageEmbed()
     .addFields(
         { name: '!help', value: 'Lists all of the commands available.' },
         { name: '!timer', value: 'Example:\n' +
-                '!timer "RSX" 16:56"\n' +
+                '!timer "RSX" 16:56\n' +
                 '\n' +
                 'Sending a message with the format:\n' +
                 '[!timer] ["MVP Name"] [Time of of death] [Optional screenshot of the Tomb]\n' +
@@ -14,7 +14,7 @@ const helpEmbed = new Discord.MessageEmbed()
                 'Sets a timer for the specific MVP. Common nick names or abbreviations like "Moonie" and "RSX" are allowed. ' +
                 'Note that the time of death is in server time, so you want to use the time on the tomb. \n' +
                 '\n' +
-                'I will remind you 15 minutes before and again 5 minutes before the MVPs spawn window.'}
+                'I will remind you 10 minutes before the MVPs spawn window.'}
     )
 
 module.exports = { helpEmbed };
